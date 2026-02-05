1a477fa0504dc2fc96a1be630378f28e81b607a5
Stephan Krusche <krusche@tum.de>
2026-02-05 13:31:52 +0100

    fix server style

--- src/test/java/de/tum/cit/aet/usermanagement/web/ResearchGroupResourceTest.java
+++ src/test/java/de/tum/cit/aet/usermanagement/web/ResearchGroupResourceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
