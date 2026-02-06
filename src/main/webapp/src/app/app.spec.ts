37de936b3b158868040e881df9f8e844823a8f16
Stephan Krusche <krusche@tum.de>
2026-02-06 11:46:08 +0100

    show version in footer

--- src/main/webapp/src/app/app.spec.ts
+++ src/main/webapp/src/app/app.spec.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { App } from './app';
@@ -6,7 +7,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([])],
+      providers: [provideHttpClient(), provideRouter([])],
     }).compileComponents();
   });
 
--- src/main/webapp/src/app/app.ts
+++ src/main/webapp/src/app/app.ts
@@ -1,7 +1,23 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 import { SecurityStore } from './core/security';
+import { environment } from '../environments/environment';
 import { AccessDeniedComponent } from './features/access-denied/access-denied.component';
+
+interface ActuatorInfoResponse {
+  build?: {
+    version?: string;
+  };
+  git?: {
+    commit?: {
+      id?: string;
+      'id.abbrev'?: string;
+    };
+    'commit.id.abbrev'?: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -12,4 +28,45 @@ import { AccessDeniedComponent } from './features/access-denied/access-denied.co
 })
 export class App {
   protected readonly securityStore = inject(SecurityStore);
+  protected readonly releasePageUrl = 'https://github.com/ls1intum/StaffPlan/releases';
+  protected readonly buildVersion = signal<string | undefined>(undefined);
+  protected readonly shortCommitHash = signal<string | undefined>(undefined);
+  protected readonly releaseLabel = computed(() => {
+    const version = this.buildVersion();
+    const shortCommitHash = this.shortCommitHash();
+
+    if (version && shortCommitHash) {
+      return `Version ${version} (${shortCommitHash})`;
+    }
+    if (version) {
+      return `Version ${version}`;
+    }
+    return undefined;
+  });
+
+  private readonly publicHttp = new HttpClient(inject(HttpBackend));
+
+  constructor() {
+    void this.fetchBuildMetadata();
+  }
+
+  private async fetchBuildMetadata(): Promise<void> {
+    try {
+      const info = await firstValueFrom(
+        this.publicHttp.get<ActuatorInfoResponse>(`${environment.apiUrl}/actuator/info`),
+      );
+      const version = info.build?.version?.trim();
+      const commitId =
+        info.git?.commit?.['id.abbrev'] ?? info.git?.commit?.id ?? info.git?.['commit.id.abbrev'];
+
+      if (version) {
+        this.buildVersion.set(version);
+      }
+      if (commitId) {
+        this.shortCommitHash.set(commitId.substring(0, 7));
+      }
+    } catch {
+      // Info endpoint can be unavailable during local frontend-only development.
+    }
+  }
 }
